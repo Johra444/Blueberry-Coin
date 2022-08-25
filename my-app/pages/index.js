abc1eba13e9233befcbb8713f332c168333d4598
Johra <ylian.veron@gmail.com>
2022-08-26 00:29:48 +0200

    changes

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -57,6 +57,7 @@ export default function Home() {
           if (amount > 0) {
           const toBeClaim = parseInt(amount, 16);
           setTokensToBeClaimed(toBeClaim);
+          setLoading2(false);
           setEligibility(true);
           return true;
           }
@@ -152,7 +153,7 @@ const getBalanceOfBlueberryTokens = async () => {
     return (
       <button className={`${styles.transacButton} ${styles.loadButton} ${prop}`}>
       <svg className={styles.load} version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-              viewBox="0 0 100 100" enable-background="new 0 0 0 0" xmlSpace="preserve">
+              viewBox="0 0 100 100" enableBackground="new 0 0 0 0" xmlSpace="preserve">
                 <path fill="#010101" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
                   <animateTransform 
                     attributeName="transform" 
@@ -225,7 +226,7 @@ const getBalanceOfBlueberryTokens = async () => {
       }
       if (tokensToBeClaimed > 0) {
         return (
-          <div className={styles.center}>
+          <div className={styles.centerClaim}>
             <div className={styles.description}>
               {tokensToBeClaimed * 10} Tokens can be claimed!
             </div>
@@ -294,14 +295,14 @@ const getBalanceOfBlueberryTokens = async () => {
                 </div>
               </div>
 
-              <div className={`${styles.mintSection} ${styles.pad2}`}>
+              <div className={styles.mintSection}>
               {mintSection()}
               </div>
             
               
             </div>
           ) : (
-            <div className={styles.center}>
+            <div className={styles.buttonContainer}>
               <ConnectButton />
             </div>
           )}
